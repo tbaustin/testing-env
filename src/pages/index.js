4a6ba947e815870a8f184909df6972b074085347
Taylor <tbaustin1992@gmail.com>
2018-05-04 10:03:27 -0500

    initial

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import zygote from 'zygote-cart'
+
+export default class Index extends React.Component {
+  componentDidMount() {
+    zygote.site('test')
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={() => zygote.toggleCart()}>Cart</button>
+        <zygote.ZygoteContainer />
+        <style jsx>{`
+          button {
+            background-color: #444;
+            color: #fff;
+            border: none;
+            outline: none;
+            cursor: pointer;
+            border-radius: 5px;
+          }
+          button:hover {
+            background-color: #fff;
+            color: #444;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
